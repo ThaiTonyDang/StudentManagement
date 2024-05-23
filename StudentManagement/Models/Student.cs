4c2616ceb5de269ca723d8953b1b756267d7f066
Noah.Dang <noah.dang@avepoint.com>
2024-05-23 14:52:40 +0700

    [APTECH] Student Management

---
+++ StudentManagement/Models/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentManagement.Models
+{
+    public class Student
+    {
+        public int StudentID { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public double GPA { get; set; }
+    }
+}
